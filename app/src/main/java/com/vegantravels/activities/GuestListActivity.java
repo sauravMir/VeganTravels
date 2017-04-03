dbe01f422aeb464e10a5b24e86e5079db1d39946
tajmulalam <taj.cse37@gmail.com>
2017-04-03 14:11:32 +0600

    gustlistactivity added

---
+++ app/src/main/java/com/vegantravels/activities/GuestListActivity.java
@@ -0,0 +1,15 @@
+package com.vegantravels.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vegantravels.R;
+
+public class GuestListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_list);
+    }
+}
