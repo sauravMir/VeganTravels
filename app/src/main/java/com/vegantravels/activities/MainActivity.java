6801f5be4350317fe84a4059e7743eeec53942a5
tajmulalam <taj.cse37@gmail.com>
2017-04-03 14:06:53 +0600

    simple changes

--- app/src/main/java/com/vegantravels/activities/MainActivity.java
+++ app/src/main/java/com/vegantravels/activities/MainActivity.java
@@ -33,6 +33,10 @@ public class MainActivity extends BaseActivity {
         Cruises cruises3 = new Cruises(4, "cruise 4", "ship 4", "2-2-2017", "12.00pm");
         Cruises cruises4 = new Cruises(5, "cruise 5", "ship 5", "2-2-2017", "12.00pm");
         Cruises cruises5 = new Cruises(6, "cruise 6", "ship 6", "2-2-2017", "12.00pm");
+        Cruises cruises6 = new Cruises(7, "cruise 7", "ship 7", "2-2-2017", "12.00pm");
+        Cruises cruises7 = new Cruises(8, "cruise 8", "ship 8", "2-2-2017", "12.00pm");
+        Cruises cruises8 = new Cruises(9, "cruise 9", "ship 9", "2-2-2017", "12.00pm");
+        Cruises cruises9 = new Cruises(10, "cruise 10", "ship 10", "2-2-2017", "12.00pm");
 
         cruisesList = new ArrayList<>();
         cruisesList.add(cruises);
@@ -41,6 +45,10 @@ public class MainActivity extends BaseActivity {
         cruisesList.add(cruises3);
         cruisesList.add(cruises4);
         cruisesList.add(cruises5);
+        cruisesList.add(cruises6);
+        cruisesList.add(cruises7);
+        cruisesList.add(cruises8);
+        cruisesList.add(cruises9);
         cruisesAdapter=new CruisesAdapter(this,cruisesList);
         lvCruises.setAdapter(cruisesAdapter);
     }
