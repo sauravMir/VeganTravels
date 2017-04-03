940b3c9c2eac4982d024ef2333985f55d869f746
Ibrar Tanim Rokan <ibrar.tanim@gmail.com>
2017-04-03 12:18:20 +0600

    Activity created

---
+++ app/src/main/java/com/vegantravels/AddExcursionActivity.java
@@ -0,0 +1,13 @@
+package com.vegantravels;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddExcursionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_excursion);
+    }
+}
