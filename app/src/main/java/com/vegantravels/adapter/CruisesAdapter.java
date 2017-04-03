0368e0d54dc9bfb4a5e1222f523c18d168dc65f3
tajmulalam <taj.cse37@gmail.com>
2017-04-03 12:47:20 +0600

    simple changes

--- app/src/main/java/com/vegantravels/adapter/CruisesAdapter.java
+++ app/src/main/java/com/vegantravels/adapter/CruisesAdapter.java
@@ -1,10 +1,13 @@
 package com.vegantravels.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
+import com.vegantravels.R;
 import com.vegantravels.model.Cruises;
 
 import java.util.ArrayList;
@@ -16,10 +19,12 @@ import java.util.ArrayList;
 public class CruisesAdapter extends BaseAdapter {
     private Context context;
     private ArrayList<Cruises> cruisesList;
+    private LayoutInflater inflater;
 
     public CruisesAdapter(Context context, ArrayList<Cruises> cruisesList) {
         this.context = context;
         this.cruisesList = cruisesList;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -37,8 +42,29 @@ public class CruisesAdapter extends BaseAdapter {
         return 0;
     }
 
+    static class ViewHolder {
+
+        TextView tvCruiseName;
+        TextView tvShipName;
+        TextView tvDate;
+        TextView tvTime;
+
+    }
+
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        ViewHolder holder = null;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.cruise_cell, null);
+            holder.tvCruiseName = (TextView) convertView.findViewById(R.id.tvCruiseName);
+            convertView.setTag(holder);
+
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+
+        return convertView;
     }
 }
