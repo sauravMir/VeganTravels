d73cff217a265a956ae83542871efe410e7fdb75
Ibrar Tanim Rokan <ibrar.tanim@gmail.com>
2017-04-05 13:23:01 +0600

    Updated

--- app/src/main/java/com/vegantravels/activities/AddCruizeActivity.java
+++ app/src/main/java/com/vegantravels/activities/AddCruizeActivity.java
@@ -1,19 +1,35 @@
 package com.vegantravels.activities;
 
 import android.app.ProgressDialog;
+import android.content.res.AssetManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.itextpdf.text.pdf.codec.Base64;
 import com.vegantravels.R;
 import com.vegantravels.asynctask.GenericsAsyncTask;
 import com.vegantravels.dialog.DialogNavBarHide;
 import com.vegantravels.manager.DatabaseManager;
 
-public class AddCruizeActivity extends BaseActivity {
+import java.io.InputStream;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+
+public class AddCruizeActivity extends BaseActivity implements View.OnClickListener {
 
     private ProgressDialog progressDialog;
     private AddCruizeActivity activity;
     private DatabaseManager databaseManager;
+    private Button btnCabinUpload;
+    private TextView tvCabinUpload;
+    EditText edtCruzeName;
 
     @Override
 
@@ -23,9 +39,56 @@ public class AddCruizeActivity extends BaseActivity {
         activity = this;
         progressDialog = new ProgressDialog(activity);
         databaseManager = new DatabaseManager(activity);
+        btnCabinUpload = (Button) findViewById(R.id.btnCabinUpload);
+        tvCabinUpload = (TextView) findViewById(R.id.tvCabinUpload);
+        edtCruzeName = (EditText) findViewById(R.id.edtCruzeName);
+        btnCabinUpload.setOnClickListener(this);
+        //findViewById();
 //        GenericsAsyncTask cruisesGenericsAsyncTask = new GenericsAsyncTask(activity, null, "inserting");
     }
 
+   /* private void findViewById() {
+        btnCabinUpload = (Button) findViewById(R.id.btnCabinUpload);
+        tvCabinUpload = (TextView) findViewById(R.id.tvCabinUpload);
+        btnCabinUpload.setOnClickListener(this);
+
+    }*/
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            /*case R.id.btnCabinUpload:
+                Toast.makeText(activity, "Toast", Toast.LENGTH_SHORT).show();
+                setXLS();
+                break;*/
+        }
+    }
+
+
+    public void setXLS(View view) {
+        try {
+            AssetManager assetManager = getAssets();
+            InputStream inputStream = assetManager.open("abc.xls");
+            Workbook workbook = Workbook.getWorkbook(inputStream);
+            Sheet sheet = workbook.getSheet(0);
+            int row = sheet.getRows();
+            int col = sheet.getColumns();
+            String values = "";
+
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; i < col; j++) {
+                    Cell cell = sheet.getCell(j, i);
+                    values = values + cell.getContents();
+                }
+                values = values + "\n";
+            }
+            edtCruzeName.setText(values);
+           // tvCabinUpload.setText(values);
+
+        } catch (Exception e) {
+
+        }
+    }
 
     class NewCruiseAsyncTask extends AsyncTask<Void, Void, Void> {
 
