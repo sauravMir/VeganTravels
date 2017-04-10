fd0d447df272c772276f44233a603ba91d8d9e56
tajmulalam <taj.cse37@gmail.com>
2017-04-10 11:54:20 +0600

    export activity created

---
+++ app/src/main/java/com/vegantravels/activities/ExportActivity.java
@@ -0,0 +1,20 @@
+package com.vegantravels.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.vegantravels.R;
+
+public class ExportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_export);
+    }
+
+    public void exportXls(View view) {
+        
+    }
+}
