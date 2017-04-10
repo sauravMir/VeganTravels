9f2adbf5a7a65d45f021a47d1a7183c7f0c8a44f
Ibrar Tanim Rokan <ibrar.tanim@gmail.com>
2017-04-10 11:40:13 +0600

    Finance activity working..

--- app/src/main/java/com/vegantravels/activities/FinanceActivity.java
+++ app/src/main/java/com/vegantravels/activities/FinanceActivity.java
@@ -4,49 +4,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.vegantravels.R;
 
-public class FinanceActivity extends BaseActivity {
+public class FinanceActivity extends BaseActivity implements View.OnClickListener {
     FinanceActivity activity;
     TextView tvCabinMain, tvPaymetMain;
     ImageButton ibtnBackFinace;
+    ListView lvFinance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finance);
         activity = this;
-        tvCabinMain = (TextView) findViewById(R.id.tvCabinMain);
-        tvPaymetMain = (TextView) findViewById(R.id.tvPaymetMain);
+
+        findViewById();
+    }
+
+    private void findViewById() {
         ibtnBackFinace = (ImageButton) findViewById(R.id.ibtnBackFinace);
+        lvFinance = (ListView) findViewById(R.id.lvFinance);
+        ibtnBackFinace.setOnClickListener(this);
+    }
 
-        tvCabinMain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(activity, ExcursionListActivity.class));
-                finishTheActivity();
-
-            }
-        });
-
-
-        tvPaymetMain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        ibtnBackFinace.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ibtnBackFinace:
                 startActivity(new Intent(activity, ManagementActivity.class));
                 finishTheActivity();
-            }
-        });
-
+                break;
+        }
     }
 
     private void finishTheActivity() {
---
+++ app/src/main/java/com/vegantravels/adapter/FinanceAdapter.java
@@ -0,0 +1,8 @@
+package com.vegantravels.adapter;
+
+/**
+ * Created by ibrar on 4/10/2017.
+ */
+
+public class FinanceAdapter {
+}
