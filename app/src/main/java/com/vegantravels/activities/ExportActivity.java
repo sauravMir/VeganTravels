35c3cff914e089b7123157788b540460fb25f136
tajmulalam <taj.cse37@gmail.com>
2017-04-10 13:51:33 +0600

    xls method added by mir vai

--- app/src/main/java/com/vegantravels/activities/ExportActivity.java
+++ app/src/main/java/com/vegantravels/activities/ExportActivity.java
@@ -6,9 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.vegantravels.R;
+import com.vegantravels.manager.DatabaseManager;
+import com.vegantravels.model.CabinModel;
 import com.vegantravels.utilities.StaticAccess;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 
 import jxl.Workbook;
@@ -19,10 +23,15 @@ import jxl.write.WritableWorkbook;
 
 public class ExportActivity extends AppCompatActivity {
 
+    private DatabaseManager databaseManager;
+    private ArrayList<CabinModel> cabinList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_export);
+        databaseManager = new DatabaseManager(ExportActivity.this);
+
     }
 
     public void exportXls(View view) {
@@ -69,13 +78,34 @@ public class ExportActivity extends AppCompatActivity {
             sheet.addCell(label7);
             sheet.addCell(label8);
 
+            for (int i = 0; i < cabinList.size(); i++) {
 
 
 
-            Label m_idValue = new Label(0, 1, "1");
+                Label m_idValue1 = new Label(0, i, String.valueOf(cabinList.get(i).getCabinNum()));
+                Label m_idValue2 = new Label(1, i, cabinList.get(i).getLName());
+                Label m_idValue3 = new Label(2, i, cabinList.get(i).getFName());
+                Label m_idValue4 = new Label(3, i, cabinList.get(i).getExcursionName());
+                Label m_idValue5 = new Label(4, i, cabinList.get(i).getExcursionDate());
+                Label m_idValue6 = new Label(5, i, String.valueOf(cabinList.get(i).getPeople()));
+                Label m_idValue7 = new Label(6, i, cabinList.get(i).getPricePerPeople());
+                Label m_idValue8 = new Label(7, i, StaticAccess.getPaymentByName(cabinList.get(i).getStatus()));
+
+                sheet.addCell(m_idValue1);
+                sheet.addCell(m_idValue2);
+                sheet.addCell(m_idValue3);
+                sheet.addCell(m_idValue4);
+                sheet.addCell(m_idValue5);
+                sheet.addCell(m_idValue6);
+                sheet.addCell(m_idValue7);
+                sheet.addCell(m_idValue8);
+            }
+
+
+           /* Label m_idValue = new Label(0, 1, "1");
             Label m_idValue1 = new Label(1, 1, "Music");
             sheet.addCell(m_idValue);
-            sheet.addCell(m_idValue1);
+            sheet.addCell(m_idValue1);*/
             m_workbook.write();
             m_workbook.close();
         } catch (Exception e) {
@@ -83,4 +113,25 @@ public class ExportActivity extends AppCompatActivity {
         }
 
     }
+
+    public ArrayList<Integer> getUniqCaninNumber(){
+        ArrayList<Integer> resList=new ArrayList<>();
+
+
+
+
+        return  resList;
+    }
+
+    /////grouping the already sent cabin list
+    public HashMap<Integer, ArrayList<CabinModel>> sortCabin(){
+        HashMap<Integer, ArrayList<CabinModel>> sortCabin= new HashMap<>();
+
+        ArrayList<Integer> uniqCabin=new ArrayList<>();
+
+
+
+        return sortCabin;
+    }
+
 }
